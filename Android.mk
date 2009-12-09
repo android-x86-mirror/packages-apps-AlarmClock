ecec34c77ea895111273225d4eb390e8fbd74b44
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-12-09 17:22:58 +0800

    an example to use build-package

--- Android.mk
+++ Android.mk
@@ -1,8 +1,1 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_PACKAGE_NAME := AlarmClock
-
-include $(BUILD_PACKAGE)
+$(call build-package)
